656d47c627452df4243dad8e041263ca01f83cd2
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2024-11-07 15:57:40 -0500

    Add documentation

--- import_event.py
+++ import_event.py
@@ -3,6 +3,13 @@ from sqlalchemy import create_engine
 
 
 def import_event_data(csv_path: str, db_path: str) -> None:
+    """
+    Import data for an event into the database.
+
+    :param csv_path: Path to event CSV
+    :param db_path: Database URL
+    """
+
     event_df = pd.read_csv(csv_path)
     engine = create_engine(db_path)
     event_df.to_sql('events', engine, if_exists='append', index=False)
--- import_players.py
+++ import_players.py
@@ -3,6 +3,13 @@ from sqlalchemy import create_engine
 
 
 def import_player_data(csv_path: str, db_path: str) -> None:
+    """
+    Import data for all players into the database.
+
+    :param csv_path: Path to players CSV
+    :param db_path: Database URL
+    """
+
     players_df = pd.read_csv(csv_path)
     engine = create_engine(db_path)
     players_df.to_sql('players', engine, if_exists='append', index=False)
