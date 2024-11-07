61e39273e3e27fd252c6e2080dd212f240a0a631
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2024-11-07 03:11:22 -0500

    Minor fixes

--- import_event_results.py
+++ import_event_results.py
@@ -1,10 +1,8 @@
-import os.path
-
 import pandas as pd
 from sqlalchemy import create_engine
 
 
-def import_event_results_data(csv_path, db_path) -> None:
+def import_event_results_data(csv_path: str, db_path: str) -> None:
     event_df = pd.read_csv(csv_path)
     engine = create_engine(db_path)
     event_df.to_sql('event_results', engine, if_exists='append', index=False)
--- import_players.py
+++ import_players.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def import_player_data(csv_path, db_path) -> None:
+def import_player_data(csv_path: str, db_path: str) -> None:
     players_df = pd.read_csv(csv_path)
     engine = create_engine(db_path)
     players_df.to_sql('players', engine, if_exists='append', index=False)
