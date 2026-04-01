0aa01116d0d8042a6301ae096d667282c403e181
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2026-04-01 16:02:11 -0400

    Adjust regions

--- init_tables.sql
+++ init_tables.sql
@@ -47,46 +47,48 @@ CREATE TABLE IF NOT EXISTS event_types_log (
 
 --region Regions
 CREATE TABLE IF NOT EXISTS regions (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY,
     region TEXT UNIQUE NOT NULL
 );
 
-INSERT INTO regions (region)
+INSERT INTO regions (id, region)
 VALUES
-    ('Québec (Greater Montreal)'),
-    ('Québec (Capitale-Nationale—Chaudière-Appalaches)'),
-    ('Québec (Outaouais)'),
-    ('Québec (Montérégie—Estrie—Centre-du-Québec)'),
-    ('Québec (Laurentides—Lanaudière—Mauricie)'),
-    ('Québec (Saguenay—Lac-Saint-Jean)'),
-    ('Québec (Bas-Saint-Laurent—Gaspésie)'),
-    ('Québec (Abitibi-Témiscamingue)'),
-    ('Québec (Côte-Nord—Nord-du-Québec)'),
-    ('Ontario (Central)'),
-    ('Ontario (East)'),
-    ('Ontario (West)'),
-    ('Ontario (North)'),
-    ('British Columbia (South West—Vancouver Island)'),
-    ('British Columbia (South Central—South East)'),
-    ('British Columbia (North)'),
-    ('Nova Scotia'),
-    ('New Brunswick'),
-    ('Prince Edward Island'),
-    ('Newfoundland and Labrador'),
-    ('Manitoba'),
-    ('Saskatchewan'),
-    ('Alberta'),
-    ('Northwest Territories'),
-    ('Yukon'),
-    ('Nunavut'),
-    ('United States'),
-    ('Europe'),
-    ('Japan'),
-    ('Asia'),
-    ('Oceania'),
-    ('South America'),
-    ('Canadian living abroad'),
-    ('Other');
+    (1, 'Québec (Greater Montreal)'),
+    (2, 'Québec (Capitale-Nationale—Chaudière-Appalaches)'),
+    (3, 'Québec (Outaouais)'),
+    (4, 'Québec (Montérégie—Estrie—Centre-du-Québec)'),
+    (5, 'Québec (Laurentides—Lanaudière—Mauricie)'),
+    (6, 'Québec (Saguenay—Lac-Saint-Jean)'),
+    (7, 'Québec (Bas-Saint-Laurent—Gaspésie)'),
+    (8, 'Québec (Abitibi-Témiscamingue)'),
+    (9, 'Québec (Côte-Nord—Nord-du-Québec)'),
+    (10, 'Ontario (Central)'),
+    (11,'Ontario (East)'),
+    (12,'Ontario (Golden Horseshoe)'),
+    (13,'Ontario (North East)'),
+    (14,'Ontario (North West)'),
+    (15,'Ontario (South West)'),
+    (20,'British Columbia (South West—Vancouver Island)'),
+    (21,'British Columbia (South Central—South East)'),
+    (22,'British Columbia (North)'),
+    (30,'Nova Scotia'),
+    (31,'New Brunswick'),
+    (32,'Prince Edward Island'),
+    (33,'Newfoundland and Labrador'),
+    (40,'Manitoba'),
+    (41,'Saskatchewan'),
+    (42,'Alberta'),
+    (50,'Northwest Territories'),
+    (51,'Yukon'),
+    (52,'Nunavut'),
+    (60,'United States'),
+    (70,'Europe'),
+    (80,'Japan'),
+    (100,'Asia'),
+    (110,'Oceania'),
+    (120,'South America'),
+    (0,'Canadian living abroad'),
+    (200,'Other');
 
 CREATE TABLE IF NOT EXISTS regions_log (
     id SERIAL PRIMARY KEY,
@@ -225,8 +227,7 @@ CREATE TABLE IF NOT EXISTS event_results (
     player_last_name TEXT NOT NULL,
     placement INT NOT NULL,
     score NUMERIC(10, 2),
-    UNIQUE (event_id, player_id),
-    UNIQUE (event_id, placement)
+    UNIQUE (event_id, player_id)
 );
 
 CREATE TABLE IF NOT EXISTS event_results_log (
